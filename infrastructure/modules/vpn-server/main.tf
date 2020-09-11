43ebbd39ebb86e6af4de97b41e140fe4a912dc6e
Paul <poginni@gmail.com>
2020-09-10 23:10:32 -0700

    Added vpn key.

--- infrastructure/modules/vpn-server/main.tf
+++ infrastructure/modules/vpn-server/main.tf
@@ -1,8 +1,11 @@
 resource "aws_instance" "vpn-server" {
   ami           = "ami-0b33356b362c56df5"
   instance_type = "t3.micro"
+  key_name = "vpn-dev"
 
   tags = {
     Name = "vpn-sever"
   }
 }
+
+
