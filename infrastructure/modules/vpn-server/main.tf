6067cc60eea2ee797b759d8d92495dd4eeab2dd3
Paul <poginni@gmail.com>
2020-09-14 02:43:35 -0700

    Added destroy option to Makefile and added userdata.

--- infrastructure/modules/vpn-server/main.tf
+++ infrastructure/modules/vpn-server/main.tf
@@ -1,10 +1,14 @@
+data "template_file" "user_data" {
+	template = "${file("../../user_data.tpl")}"
+}
+
 resource "aws_instance" "vpn-server" {
   ami           = "ami-0b33356b362c56df5"
   instance_type = "t3.micro"
   key_name = "vpn-dev"
 
   tags = {
-    Name = "vpn-sever"
+    Name = "wireguard-2"
   }
 }
 
